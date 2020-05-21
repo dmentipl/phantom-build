bcfa5b1a68d2606f2075e86aece99fccfdc509c8
Daniel Mentiplay <d.mentiplay@gmail.com>
2020-05-21 14:12:24 +1000

    Fix setup_calculation test

--- tests/test_phantombuild.py
+++ tests/test_phantombuild.py
@@ -88,23 +88,20 @@ def test_build_phantom():
             pb.build_phantom(**kwargs)
 
 
-@pytest.mark.skip(reason='This test fails on Travis CI; not sure why...')
 def test_setup_calculation():
     """Test setting up Phantom calculation."""
     with tempfile.TemporaryDirectory() as tmpdirname:
         phantom_path = Path(tmpdirname) / 'phantom'
-        run_dir = Path(tmpdirname) / 'run_dir'
+        run_path = Path(tmpdirname) / 'run_path'
         input_dir = Path(__file__).parent / 'stub'
         in_file = input_dir / 'disc.in'
         setup_file = input_dir / 'disc.setup'
         pb.get_phantom(phantom_path)
-        pb.build_phantom(
-            path=phantom_path, setup='disc', system='gfortran',
-        )
+        pb.build_phantom(path=phantom_path, setup='disc', system='gfortran')
         pb.setup_calculation(
             prefix='disc',
-            run_dir=run_dir,
             setup_file=setup_file,
             in_file=in_file,
+            run_path=run_path,
             phantom_path=phantom_path,
         )
